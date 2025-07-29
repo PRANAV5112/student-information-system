efe0fe594c2a32423173e0f9d4ce835ed27459c1
Pranathijaina <pranathijaina@gmail.com>
2025-07-29 17:12:03 +0530

    Configure project for Render deployment with createsu command

---
+++ students/management/commands/createsu.py
@@ -0,0 +1,22 @@
+# students/management/commands/createsu.py
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    help = 'Creates a superuser from environment variables'
+
+    def handle(self, *args, **options):
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+        
+        if not User.objects.filter(username=username).exists():
+            self.stdout.write(f'Creating account for {username} ({email})')
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password
+            )
+        else:
+            self.stdout.write('Superuser already exists.')
